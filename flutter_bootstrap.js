c64bde41a19dcce0da718c1171f2072543e5d26a
corewave-dev <info.corewave@gmail.com>
2025-12-01 14:07:57 +0530

    update-design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2107343561"
+    serviceWorkerVersion: "1310848348"
   }
 });
