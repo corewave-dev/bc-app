a35846bdcc3f3adde45c03d422224ef1f8163b90
corewave-dev <info.corewave@gmail.com>
2025-12-03 00:07:21 +0530

    update-buttons-&-validate

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "4e74c38f8d8aa7ea0dcb35002ad572d8
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "0fd0777220fa570529a4ac79ca522b3e",
+"flutter_bootstrap.js": "b49eb371e2eaae2bb64e75e0a16d6f97",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "9e7c2e9ee4ca957fbe7ff6d46a6538ec",
 "/": "9e7c2e9ee4ca957fbe7ff6d46a6538ec",
-"main.dart.js": "96087c9ddd909268e353879d640bfd9f",
+"main.dart.js": "1042dcc5f97533c95160d00ba8df963c",
 "manifest.json": "ba6fd497f67279f922a2e9a9e5c3a1dc",
 "version.json": "5e4bc0f167298bbe92772b71279aa595"};
 // The application shell files that are downloaded before a service worker can
